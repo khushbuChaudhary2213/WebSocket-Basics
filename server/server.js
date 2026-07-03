1a37c11a1394122afd87c7bc44f982ca48554372
Khushbu <khushbuchaudhary2213@gmail.com>
2026-07-03 19:08:02 +0530

    Learned some more concepts

--- server/server.js
+++ server/server.js
@@ -4,26 +4,48 @@ const http = require("http");
 const express = require("express");
 const { Server } = require("socket.io");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookie = require("cookie");
+
+const port = 9000;
+const secretKey = "qwerty";
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: process.env.FRONTEND_URL,
+    credentials: true,
   },
 });
 
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
+    credentials: true,
   }),
 );
 
+const user = "khushbu";
+
 // middleware
 io.use((socket, next) => {
-  // Like can be used for authentication!
+  const cookies = cookie.parse(socket.request.headers.cookie || "");
 
-  next();
+  const token = cookies.token;
+  if (!token) {
+    return next(new Error("Authentication Error"));
+  }
+
+  try {
+    const decoded = jwt.verify(token, secretKey);
+    next();
+  } catch (err) {
+    next(new Error("Invalid Token"));
+  }
+
+  // can be used for authentication!
+  // if (user == "khushbu") next();
 });
 
 // Socket.io
@@ -56,13 +78,33 @@ io.on("connection", (socket) => {
   // });
 });
 
+///////////////////////////
 // HTTP Requests
 app.use(express.static("./public"));
 
 app.get("/", (req, res) => {
-  return res.sendFile("/public/index.html");
+  res.status(200).json({
+    status: true,
+    message: "Welcome For Learning WebSocket!",
+  });
 });
 
-server.listen(9000, () => {
-  console.log(`Server started at PORT: 9000`);
+app.get("/login", (req, res) => {
+  const token = jwt.sign({ _id: "asdfghjkl" }, secretKey);
+
+  // console.log("Generated token:", token);
+  res
+    .cookie("token", token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax",
+    })
+    .json({
+      status: true,
+      message: "Login Successfully",
+    });
+});
+
+server.listen(port, () => {
+  console.log(`Server started at PORT: ${port}`);
 });
